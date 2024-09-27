9293266f5dbf413d66b6d9f448bb6403593cdcd8
Meredith Pappa <meredithpappa@gmail.com>
2024-09-27 13:10:36 +0300

    Added JS icon API

--- script.js
+++ script.js
@@ -4,9 +4,12 @@ function refreshWeather(response) {
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
-  let windSpeedElement = documenty.querySelector("#wind-speed");
+  let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
+  
+  iconElement.innerHTML = `<img src= "${response.data.condition.icon_url}" class="weather-app-icon" />;
 
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
